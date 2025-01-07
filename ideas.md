c01c0e61031f33c2a61717a049f5a427f89d65fa
Asiman <asiman.ismayilova.2006@mail.ru>
2025-01-07 23:18:16 +0400

    Possible enhancements & finish with logic/structure

---
+++ ideas.md
@@ -0,0 +1,5 @@
+# Enhancement Ideas
+
+- [ ] Database migration with Flyway for following history of changes in database.
+- [ ] Use caching to find the user by his email address to improve the performance at JwtAuthenticationFilter.
+- [ ] Apply any validation in the DTO .
--- src/main/java/com/example/SignupLoginApplication.java
+++ src/main/java/com/example/SignupLoginApplication.java
@@ -1,4 +1,4 @@
-package com.example.signupLogin;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class SignupLoginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SignupLoginApplication.class, args);
 	}
-
 }
