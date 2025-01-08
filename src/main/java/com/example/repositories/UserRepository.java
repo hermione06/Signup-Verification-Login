171a74f72b1feff8df9d660d95c21ed292d77cd0
Asiman <asiman.ismayilova.2006@mail.ru>
2025-01-08 22:02:54 +0400

    feat: implement findByVerificationCode in UserRepository

--- src/main/java/com/example/repositories/UserRepository.java
+++ src/main/java/com/example/repositories/UserRepository.java
@@ -1,14 +1,13 @@
 package com.example.repositories;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
     
 }
