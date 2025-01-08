d3fdecbdc6addca10856f7ded541ee4b41ec172d
Asiman <asiman.ismayilova.2006@mail.ru>
2025-01-08 21:49:37 +0400

    add java mail sender dependency to build file add fields related with verification code to User class add mail properties

--- ideas.md
+++ ideas.md
@@ -3,3 +3,4 @@
 - [ ] Database migration with Flyway for following history of changes in database.
 - [ ] Use caching to find the user by his email address to improve the performance at JwtAuthenticationFilter.
 - [ ] Apply any validation in the DTO .
+- [ ] Implement Role-based Access Control.
--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -1,10 +1,8 @@
 package com.example.entities;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,57 +14,43 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
-@Entity
-@Table(name="users")
-public class User implements UserDetails{
     
-    @Getter
-    @Setter
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(nullable=false)    
-    private Integer id;
-
-    @Getter
-    @Setter
-    @Column(nullable = false)
-    private String fullName;
-    
-    @Getter
-    @Setter
-    @Column(unique = true, length = 100, nullable = false)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
     private String email;
-
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String password;
 
-    @Getter
-    @Setter
-    @CreationTimestamp
-    @Column(updatable = false, name = "created_at")
-    private Date createdAt;
+    @Column(name = "verification_code")
+    private String verificationCode;
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationCodeExpiresAt;
+    private boolean enabled;
 
-    @Getter
-    @Setter
-    @UpdateTimestamp
-    @Column(name = "updated_at")
-    private Date updatedAt;
-
-    @Override
-    public Collection <? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-        //This method supposed to return the user's roles list,
-        // for now it returns empty list.
+    // Constructor for creating an unverified user
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
     }
-    
-    @Override
-    public String getUsername() {
-        return email;
-    }//Note: returns email, as it is unique user info!
+    // Default constructor
+    public User(){
+    }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+    // Proper checks !!!!
     @Override
     public boolean isAccountNonExpired() {
         return true;
@@ -84,10 +68,6 @@ public class User implements UserDetails{
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return enabled;
     }
-    // To-Do: Customize logics for isAccountNonExpired, isEnabled etc methods
-    
-
-
 }
